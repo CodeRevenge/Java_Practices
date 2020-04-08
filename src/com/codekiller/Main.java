1b5f2ee9a2c1919cca38662d704126e0a6a3da9d
CodeRevenge <mas.arley2009@hotmail.com>
2020-04-07 21:44:17 -0500

    Practice 07

--- src/com/codekiller/Main.java
+++ src/com/codekiller/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         Problems problems = new Problems();
-        problems.run(4);
+        problems.run(7);
     }
 }
--- src/com/codekiller/Problems.java
+++ src/com/codekiller/Problems.java
@@ -1,5 +1,6 @@
 package com.codekiller;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Problems {
@@ -26,6 +27,19 @@ public class Problems {
                 prob05();
                 break;
             case 6:
+                prob06();
+                break;
+            case 7:
+                prob07();
+                break;
+            case 8:
+                prob08();
+                break;
+            case 9:
+                prob09();
+                break;
+            case 10:
+                prob10();
                 break;
             default:
                 break;
@@ -76,14 +90,74 @@ public class Problems {
     }
 
     void prob05() {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d", N, i, N * i);
+        }
     }
 
     void prob06() {
+        int t = scanner.nextInt();
+        ArrayList<Integer> a = new ArrayList<>();
+        ArrayList<Integer> b = new ArrayList<>();
+        ArrayList<Integer> n = new ArrayList<>();
+
+        for (int i = 0; i < t; i++) {
+            a.add(scanner.nextInt());
+            b.add(scanner.nextInt());
+            n.add(scanner.nextInt());
+        }
+
+        for (int i = 0; i < t; i++) {
+            String resultList = "";
+            for (int j = 1; j <= n.get(i); j++) {
+                int result = a.get(i);
+                for (int k = 0; k < j; k++) {
+                    result += Math.pow(2, k) * b.get(i);
+                }
+                resultList += result + " ";
+            }
+            resultList = resultList.substring(0, resultList.length() - 1);
+            System.out.println(resultList);
+        }
+
 
     }
 
     void prob07() {
+        int t = scanner.nextInt();
+
+        for (int i = 0; i < t; i++) {
+
+            try {
+                long x = scanner.nextLong();
+                System.out.println(x + " can be fitted in:");
+                if (x >= -128 && x <= 127) System.out.println("* byte");
+                if (x >= -Math.pow(2, 15) && x <= Math.pow(2, 15) - 1) System.out.println("* short");
+                if (x >= -Math.pow(2, 31) && x <= Math.pow(2, 31) - 1) System.out.println("* int");
+                if (x >= -Math.pow(2, 63) && x <= Math.pow(2, 63) - 1) System.out.println("* long");
+
+//                if (x >= -32_768 && x <= 32_767) System.out.println("* short");
+//                if (x >= -2_147_483_648 && x <= 2_147_483_647) System.out.println("* int");
+//                if (x >= -9_223_372_036_854_775_808L && x <= 9_223_372_036_854_775_807L) System.out.println("* long");
+            } catch (Exception e) {
+                System.out.println(scanner.next() + " can't be fitted anywhere.");
+            }
+
+        }
+    }
+
+    void prob08() {
+
+    }
+
+    void prob09() {
+
+    }
+
+    void prob10() {
 
     }
 }
