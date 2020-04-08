59e1197eafa01e165da0b239a5c74a236e318305
CodeRevenge <mas.arley2009@hotmail.com>
2020-04-07 19:33:28 -0500

    Practice 03

--- src/com/codekiller/Main.java
+++ src/com/codekiller/Main.java
@@ -3,6 +3,16 @@ package com.codekiller;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Instances
+//        _01_WelcomeToJava pro01 = new _01_WelcomeToJava();
+//        _02_JavaStdinAndStdoutI pro02 = new _02_JavaStdinAndStdoutI();
+        _03_JavaStdinAndStdoutII pro03 = new _03_JavaStdinAndStdoutII();
+
+
+        // Runnable
+//        pro01.run();
+//        pro02.run();
+        pro03.run();
     }
 }
